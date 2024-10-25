291708e6c10345d29431966dd8682c503407d11c
Sina Sami <sinasamii7878@gmail.com>
2024-10-25 13:06:44 +0330

    Add ESLint package/lint script and change eslint config

---
+++ online-shop_app/eslint.config.mjs
@@ -0,0 +1,41 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: globals.browser,
+    },
+
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      react: pluginReact,
+    },
+
+    rules: {
+      // Basic ESLint and TypeScript rules
+      ...pluginJs.configs.recommended.rules,
+      ...tsPlugin.configs.recommended.rules,
+      "no-console": "error",
+      // React rules without implicit "extends"
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-filename-extension": [
+        "warn",
+        { extensions: [".js", ".jsx", ".tsx", ".ts"] },
+      ],
+    },
+  },
+];
